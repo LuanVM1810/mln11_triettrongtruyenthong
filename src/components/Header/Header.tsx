c4e5757e159079dc4d087addb02a5aa82a475618
LuanVM1810 <Vomongluan@gmail.com>
2024-09-24 20:29:43 +0700

    search component in mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,6 +25,16 @@ const Header = ({ location }: Props) => {
     <nav className="font-vietnam fixed w-full z-10 text-black">
       <div className="text-[24px] flex items-center w-full px-4 py-4 lg:px-8 lg:py-6 lg:text-[30px]">
         <FaTwitter size={24} />
+        <div className="mx-3 lg:hidden p-1 overflow-hidden w-[30px] h-[30px] hover:w-[270px] text-black hover:shadow-md rounded-full flex group items-center hover:duration-300 duration-300">
+          <div className="flex items-center justify-center">
+            <IoIosSearch size={24} />
+          </div>
+          <input
+            type="text"
+            className="outline-none text-[14px] bg-transparent w-full text-black font-normal px-4"
+          />
+        </div>
+
         <IoMenu
           onClick={onClickOpenMobileMenu}
           className="ml-auto lg:hidden text-black"
