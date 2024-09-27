a5d87ae8f38f38fbfa77ceee75ad30d46a915dfd
LuanVM1810 <Vomongluan@gmail.com>
2024-09-27 16:42:38 +0700

    add quiz component

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,6 +1,56 @@
+import { useState } from "react";
 import NewsImage from "../../assets/Background trang bạn học triết.png";
+import { data1 } from "../../constants";
+import NenKetQua from "../../assets/nền kết quả_ 1.png";
+import { IoMdHome } from "react-icons/io";
+import { NavLink } from "react-router-dom";
+
+interface Question {
+  question: string;
+  option1: string;
+  option2: string;
+  option3: string;
+  ans: number;
+}
 
 const Quiz = () => {
+  const [index, setIndex] = useState(0);
+  const [chooseAnswer, setChooseAnswer] = useState<number | null>(null);
+  const [isCorrect, setIsCorrect] = useState<boolean>();
+  let question: Question | null = data1[index];
+  const [lock, setLock] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
+  const [result, setResult] = useState<boolean>(false);
+
+  const percent = (score / data1.length) * 100 + "%";
+
+  const checkAnswer = (ans: number) => {
+    if (lock == false) {
+      if (question?.ans === ans) {
+        setChooseAnswer(ans);
+        setIsCorrect(true);
+        setLock(true);
+        setScore((prev) => prev + 1);
+      } else {
+        setChooseAnswer(ans);
+        setIsCorrect(false);
+        setLock(true);
+      }
+    }
+  };
+
+  const nextQuestion = () => {
+    if (index == data1.length - 1) {
+      question = null;
+      setResult(true);
+      return 0;
+    }
+    setIndex(index + 1);
+    setChooseAnswer(null);
+    setIsCorrect(false);
+    setLock(false);
+  };
+
   return (
     <div className="pt h-screen lg:max-w-full">
       <div
@@ -9,32 +59,109 @@ const Quiz = () => {
           backgroundImage: `url(${NewsImage})`,
         }}
       >
-        <div className="bg-triethoc-green max-w-[500px] mx-4 px-4 lg:px-6 py-4 lg:py-6 rounded-[20px]">
-          <div className="flex justify-center">
-            <p className="bg-triethoc-white px-4 py-2 mb-4 rounded-md text-black/80 font-bold">
-              Cấp 1
-            </p>
-          </div>
-          <p className="uppercase text-triethoc-brown font-bold mb-4">
-            1. Chủ nghĩa Mác-Lênin gồm 3 bộ phận cấu thành đó là:
-          </p>
-          <ul className="text-white">
-            <li className="bg-[#546D68] mb-4 lg:mb-6 p-2 lg:p-4 rounded-[13px] border-2 border-white cursor-pointer hover:opacity-70">
-              a. Triết học, Kinh tế chính trị học và Chủ nghĩa xã hội khoa học
-            </li>
-            <li className="bg-[#546D68] mb-4 lg:mb-6 p-2 lg:p-4 rounded-[13px] border-2 border-white cursor-pointer hover:opacity-70">
-              b. Triết học, Kinh tế chính trị học và Chính trị học
-            </li>
-            <li className="bg-[#546D68] mb-4 lg:mb-6 p-2 lg:p-4 rounded-[13px] border-2 border-white cursor-pointer hover:opacity-70">
-              c. Triết học, Kinh tế học và Chủ nghĩa xã hội khoa học
-            </li>
-          </ul>
-          <div className="flex justify-end mb-4">
-            <button className="px-6 py-2 bg-triethoc-white font-bold rounded-[13px] text-black/80">
-              Tiếp
-            </button>
-          </div>
-          <div className="text-white text-center">1 trong 10 câu hỏi</div>
+        <div className="bg-triethoc-green shadow-btn w-[700px] max-w-[1000px] mx-4 px-4 lg:px-6 py-4 lg:py-6 rounded-[20px]">
+          {result === true ? (
+            <div className="lg:grid lg:grid-cols-2 ">
+              <div className="h-[220px] lg:h-full flex items-center justify-center">
+                <img className="" src={NenKetQua} alt="" />
+              </div>
+              <div>
+                <div className="font-harmoni text-center text-[40px] lg:text-[50px] text-triethoc-brown">
+                  Kết quả của bạn
+                </div>
+                <div className="flex justify-between items-center">
+                  <div className="text-[18px] lg:text-[28px] text-white">
+                    {score}/{data1.length} Câu đúng
+                  </div>
+                  <div className="font-bold text-black/80 px-3 py-1 uppercase bg-triethoc-white rounded-[9px]">
+                    Cấp 1
+                  </div>
+                </div>
+                <div className="w-[100%] relative h-[13px] bg-triethoc-white mt-5 rounded-[20px]">
+                  <div
+                    className={`w-[${percent}] absolute bg-triethoc-brown h-[13px] rounded-[20px]`}
+                  ></div>
+                </div>
+                <div className="mt-5 text-white">
+                  Những cá nhân hướng ngoại sẵn sàng tận hưởng các hoạt động
+                  nhóm và coi trọng việc tương tác xã hội. Họ có xu hướng nhiệt
+                  tình, hướng ngoại và thể hiện sự hào hứng của mình.
+                </div>
+
+                <div className="flex justify-end mt-5">
+                  <NavLink to="/">
+                    <button className="flex items-center p-2 bg-triethoc-white rounded-[10px] gap-1">
+                      <IoMdHome size={18} color="black" />
+                      Về trang chủ
+                    </button>
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <>
+              <div className="flex justify-center">
+                <p className="bg-triethoc-white px-4 py-2 mb-4 rounded-md text-black/80 font-bold">
+                  Cấp 1
+                </p>
+              </div>
+              <p className="uppercase text-triethoc-brown font-bold mb-4">
+                {index + 1}. {question?.question}
+              </p>
+              <ul className="text-white">
+                <li
+                  onClick={() => {
+                    checkAnswer(1);
+                  }}
+                  className={` ${
+                    chooseAnswer == 1 && isCorrect == true && `bg-[#00d397]`
+                  } ${
+                    chooseAnswer == 1 && isCorrect == false && "bg-[#ff4a4a]"
+                  } bg-[#546D68] mb-4 lg:mb-6 p-2 lg:p-4 rounded-[13px] border-2 border-white cursor-pointer`}
+                >
+                  a. {question?.option1}
+                </li>
+                <li
+                  onClick={() => {
+                    checkAnswer(2);
+                  }}
+                  className={` ${
+                    chooseAnswer == 2 && isCorrect == true && `bg-[#00d397]`
+                  } ${
+                    chooseAnswer == 2 && isCorrect == false && "bg-[#ff4a4a]"
+                  } bg-[#546D68] mb-4 lg:mb-6 p-2 lg:p-4 rounded-[13px] border-2 border-white cursor-pointer`}
+                >
+                  b. {question?.option2}
+                </li>
+                <li
+                  onClick={() => {
+                    checkAnswer(3);
+                  }}
+                  className={` ${
+                    chooseAnswer == 3 && isCorrect == true && `bg-[#00d397]`
+                  } ${
+                    chooseAnswer == 3 && isCorrect == false && "bg-[#ff4a4a]"
+                  } bg-[#546D68] mb-4 lg:mb-6 p-2 lg:p-4 rounded-[13px] border-2 border-white cursor-pointer`}
+                >
+                  c. {question?.option3}
+                </li>
+              </ul>
+              <div className="flex justify-end mb-4">
+                <button
+                  disabled={chooseAnswer === null ? true : false}
+                  onClick={nextQuestion}
+                  className={`${
+                    chooseAnswer === null && "cursor-not-allowed bg-opacity-25"
+                  } px-6 py-2 bg-triethoc-white font-bold rounded-[13px] text-black/80`}
+                >
+                  Tiếp
+                </button>
+              </div>
+              <div className="text-white text-center">
+                {index + 1} trong {data1.length} câu hỏi
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const data = [
+export const data1 = [
   {
     question: "Chủ nghĩa Mác-Lênin gồm ba bộ phận cấu thành đó là:",
     option1: "Triết học, Kinh tế chính trị học và Chủ nghĩa xã hội khoa học",
@@ -33,5 +33,96 @@ export const data = [
     option2: "Những gì tồn tại lệ thuộc vào ý thức của con người",
     option3: "Những gì tồn tại không phụ thuộc vào ý thức của con người",
     ans: 3,
+  },
+  {
+    question:
+      ' "Lý thuyết thiết lập chương trình nghị sự" (Agenda Setting Theory) trong truyền thông cho rằng:',
+    option1: "Truyền thông quyết định công chúng sẽ nghĩ gì",
+    option2: "Truyền thông không ảnh hưởng đến quan điểm công chúng",
+    option3: "Truyền thông ảnh hưởng đến những vấn đề mà công chúng chú ý",
+    ans: 3,
+  },
+  {
+    question:
+      "Theo triết học Mác-Lênin, phương tiện truyền thông đại chúng có vai trò gì trong quá trình hình thành ý thức xã hội?",
+    option1: "Phản ánh và củng cố tư tưởng của giai cấp cầm quyền",
+    option2: "Không có ảnh hưởng đến ý thức xã hộ",
+    option3: "Chỉ truyền tải thông tin mà không có tác động gì",
+    ans: 1,
+  },
+  {
+    question:
+      "Theo Mác-Lênin, phương tiện truyền thông đóng vai trò gì trong quá trình sản xuất tư tưởng?",
+    option1: "Tạo ra tư tưởng",
+    option2: "Lan truyền và củng cố tư tưởng",
+    option3: "Phản ánh tư tưởng của quần chúng",
+    ans: 2,
+  },
+];
+
+export const data2 = [
+  {
+    question: "Triết học Mác - Lênin do ai sáng lập và phát triển?",
+    option1: "C. Mác, Ph. Ăngghen; V.I. Lênin",
+    option2: "C. Mác và Ph. Ăngghen",
+    option3: "V.I. Lênin.",
+    ans: 1,
+  },
+  {
+    question:
+      "Triết học Mác - Lênin ra đời trong điều kiện kinh tế - xã hội nào?",
+    option1:
+      "Phương thức sản xuất tư bản chủ nghĩa đã trở thành phương thức sản xuất thống trị",
+    option2: "Phương thức sản xuất tư bản chủ nghĩa mới xuất hiện",
+    option3: " Chủ nghĩa tư bản đã trở thành chủ nghĩa đế quốc",
+    ans: 1,
+  },
+  {
+    question: "Ý thức là hình ảnh chủ quan của thế giới",
+    option1: "Hiện thực",
+    option2: "Khách quan",
+    option3: "Chủ quan",
+    ans: 2,
+  },
+  {
+    question: "Kết cấu của ý thức theo chiều dọc bao gồm:",
+    option1: "Tự ý thức, tiềm thức, vô thức",
+    option2: "Tự ý thức, tri thức, tiềm thức, vô thức",
+    option3: " Các phương án trả lời đều sai",
+    ans: 1,
+  },
+  {
+    question:
+      "Tác phẩm nào được xem là đánh dấu cho sự chín muồi về tư tưởng xã hội chủ nghĩa của C.Mác và Ăng-ghen ?",
+    option1: "Gia đình thần thánh (1842)",
+    option2: "Hệ tư tưởng Đức (1842)",
+    option3: "Tuyên ngôn của Đảng cộng sản (1848)",
+    ans: 3,
+  },
+  {
+    question:
+      "Ba phát minh lớn nhất của khoa học tự nhiên làm cơ sở khoa học tự nhiên cho sự ra đời tư duy biện chứng duy vật đầu thế kỷ XIX là những phát minh nào?",
+    option1:
+      "1) Thuyết mặt trời làm trung tâm vũ trụ của Côpécních, 2) định luật bảo toàn khối lượng của Lômônôxốp, 3) học thuyết tế bào.",
+    option2:
+      "1) Định luật bảo toàn và chuyển hoá năng lượng, 2) học thuyết tế bào, 3) học thuyết tiến hoá của Đacuyn.",
+    option3:
+      "1) Phát hiện ra nguyên tử, 2) phát hiện ra điện tử, 3) định luật bảo toàn và chuyển hoá năng lượng.",
+    ans: 1,
+  },
+  {
+    question: "Lịch sử xã hội loài người đã trải qua",
+    option1: "Bốn hình thái kinh tế xã hội",
+    option2: "Ba hình thái kinh tế xã hội",
+    option3: "Năm hình thái kinh tế xã hội",
+    ans: 3,
+  },
+  {
+    question:
+      "Theo quan điểm của chủ nghĩa duy vật biện chứng, mọi sự vật và hiện tượng trong truyền thông đều có mối quan hệ gì?",
+    option1: "Tách rời nhau và không ảnh hưởng lẫn nhau",
+    option2: "Liên hệ và tác động qua lại lẫn nhau",
+    option3: "Phụ thuộc hoàn toàn vào ý thức con người",
+    ans: 2,
   },
 ];
