b5a00ae05692657a3e1aaabfab1cd5a9f85f52fc
LuanVM1810 <Vomongluan@gmail.com>
2024-09-29 17:37:20 +0700

    fff

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -10,7 +10,9 @@ const SideBar = ({ location }: Props) => {
     <div className="hidden lg:block z-10 bg-transparent border-r-2 border-[#595858/50] fixed h-screen w-[50px] lg:w-[100px] left-0 top-0 bottom-0">
       <div className="flex flex-col items-center justify-center py-4">
         <span>
-          <img src={Logo} width={64} />
+          <NavLink to="/">
+            <img src={Logo} width={64} />
+          </NavLink>
         </span>
         <div className="flex flex-col gap-10 mt-[200px]">
           <NavLink to="/">
