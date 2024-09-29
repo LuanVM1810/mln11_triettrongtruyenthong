732519edd2f1aee6c1114dc7060b1d75fcae482b
LuanVM1810 <Vomongluan@gmail.com>
2024-09-29 17:41:41 +0700

    add vercel analystic

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,11 +3,13 @@ import Header from "../components/Header/Header";
 import SideBar from "../components/SideBar/SideBar";
 import Slogan from "../components/Slogan/Slogan";
 import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 
 const HomePage = () => {
   const location = useLocation();
   return (
     <div className="text-14 h-screen font-vietnam">
+      <Analytics />
       <ScrollToTop />
       <Header location={location} />
       <SideBar location={location} />
