56afe3e73b99d8a2b2d315a72dbbcbd6d80571ce
LuanVM1810 <Vomongluan@gmail.com>
2024-10-06 15:19:01 +0700

    fix tudien

--- src/components/Tudien/Tudien.tsx
+++ src/components/Tudien/Tudien.tsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom";
 import TuDienBackground from "../../assets/Từ điển triết bg.png";
 import TudienTriethoc from "../../assets/tu dien triet hoc.png";
-import TudienTruyenthong from "../../assets/tu dien truyen thong.png";
+// import TudienTruyenthong from "../../assets/tu dien truyen thong.png";
 import { motion } from "framer-motion";
 const Tudien = () => {
   return (
     <div
       style={{ backgroundImage: `url(${TuDienBackground})` }}
-      className="h-screen w-[100%] overflow-x-hidden mt-10 lg:mt-0 z-2 relative flex flex-col lg:flex-row items-center bg-contain lg:bg-top lg:bg-cover duration-500 ease-in"
+      className="h-screen w-[100%] overflow-x-hidden mt-10 lg:mt-0 z-2 relative flex flex-col justify-center lg:flex-row items-center bg-contain lg:bg-top lg:bg-cover duration-500 ease-in"
     >
       <NavLink to="truyenthong">
         <motion.div
@@ -19,14 +19,14 @@ const Tudien = () => {
           <img src={TudienTriethoc} />
         </motion.div>
       </NavLink>
-      <motion.div
+      {/* <motion.div
         initial={{ y: -1000 }}
         animate={{ y: 0 }}
         transition={{ duration: 2.2 }}
         className="lg:absolute max-lg:px-8 lg:right-32 h-[347px] w-[420px]"
       >
         <img src={TudienTruyenthong} />
-      </motion.div>
+      </motion.div> */}
     </div>
   );
 };
